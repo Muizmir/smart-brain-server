74a84f3195a62d79576d6b6560d4ad48628e6e4e
Muiz <m.muiz.q@gmail.com>
2020-06-05 18:00:36 +1000

    creating a server

---
+++ controllers/image.js
@@ -0,0 +1,36 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'd40cbf80aa7d45a6905d41b6aa2df353'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json('Unable to work with Api Call')
+        })
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('*')
+        .then(entries => {
+            if (entries.length) {
+                res.json(entries[0].entries);
+            }
+            else {
+                res.status(400).json('Not found')
+            }
+        })
+        .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
