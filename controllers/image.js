3fc99dea65b99e2c5ac85ea2189446388048e414
Muiz <m.muiz.q@gmail.com>
2020-06-05 19:34:04 +1000

    fixed db

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,16 @@
-const clarifai = require('clarifai');
+const Clarifai = require('clarifai');
 
-const app = new clarifai.App({
+const app = new Clarifai.App({
     apiKey: 'd40cbf80aa7d45a6905d41b6aa2df353'
 });
 
 const handleApiCall = (req, res) => {
-    app.models.predict(clarifai.FACE_DETECT_MODEL, req.body.input)
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
         .then(data => {
             res.json(data);
         })
         .catch(err => {
-            res.status(400).json('Unable to work with Api Call')
+            res.status(400).json(err)
         })
 }
 
